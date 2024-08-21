d06a06a7f4c73aa56c40c8b79d26746ca81c1585
Jesse David <114846268+jessed7@users.noreply.github.com>
2024-08-21 01:35:51 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const Game = dynamic(() => import("./components/game"), {ssr: false});
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <h1 className="font-bold text-2xl">Who's that Anime Character?</h1>
+      <h1 className="font-bold text-2xl">Who&apos;s that Anime Character?</h1>
       <Game />
     </main>
   );
