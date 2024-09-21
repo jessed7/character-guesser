645209a4717da3bfa1dd071def2cd405d5028cc2
Jesse David <114846268+jessed7@users.noreply.github.com>
2024-09-20 23:44:57 -0700

    Add page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 // import Game from "./components/game";
 import dynamic from "next/dynamic";
 
+export const metadata = {
+  title: "Who's that Anime Character?",
+  description: "An anime character guessing game",
+}
+
 export default function Home() {
   const Game = dynamic(() => import("./components/game"), {ssr: false});
   return (
